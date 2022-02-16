87ad624b02cc3ee2c5129b2e2b3bf8c9799e6238
ABrandaoL <alexandre.brandao.lustosa@gmail.com>
2022-02-16 20:21:51 -0300

    Print Application Name and version
    Added capacity to print application name and version core

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
+const char* application = "kiwue-server";
+const char* version = "0.0.1";
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << application << " " << version << std::endl;
     return 0;
 }
